5f9b97efaaaff57a08855cab2bce92756c52aa99
sukun <sukunrt@gmail.com>
2024-04-02 19:26:24 +0530

    improve git config

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 ln -sf $(pwd)/polybar.ini ~/.config/polybar/config.ini
 ln -sf $(pwd)/xprofile ~/.xprofile
 ln -sf $(pwd)/rofi.rasi ~/.config/rofi/config.rasi
+ln -sf $(pwd)/alacritty.toml ~/.alacritty.toml
+ln -sf $(pwd)/tmux.conf ~/.tmux.conf
+
+git config --global init.defaultBranch main
